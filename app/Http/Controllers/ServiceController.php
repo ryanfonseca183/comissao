c722a9354e8f7487e75b2db8ffec27d959b683c7
ryanfonseca183 <67619131+ryanfonseca183@users.noreply.github.com>
2023-05-07 21:28:41 -0300

    Exclusão de serviço

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -57,6 +57,8 @@ class ServiceController extends Controller
      */
     public function destroy(Service $service)
     {
-        //
+        $service->delete();
+
+        return redirect()->route('admin.services.index');
     }
 }
